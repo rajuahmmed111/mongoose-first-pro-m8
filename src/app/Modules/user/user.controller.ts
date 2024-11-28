648859c9d74e54cb5439f71db344efbb67527159
RAJU <ahraju2003@gmail.com>
2024-11-28 12:00:43 +0600

    update module in user create stedent controller in response message

--- src/app/Modules/user/user.controller.ts
+++ src/app/Modules/user/user.controller.ts
@@ -24,7 +24,7 @@ const createFaculty = catchAsync(async (req, res) => {
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: 'Faculty is created succesfully',
+    message: 'Faculty is created successfully',
     data: result,
   });
 });
@@ -37,7 +37,7 @@ const createAdmin = catchAsync(async (req, res) => {
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: 'Admin is created succesfully',
+    message: 'Admin is created successfully',
     data: result,
   });
 });
